4ae46fbd18087c1245c7de33a7caef8c134cf5f2
PABLO <PABLO@DESKTOP-Q27PB3U>
2026-05-29 10:48:35 +0200

    Agregacion - BuscaPalabra y BuscaVecesPalabra

---
+++ src/Pablo/BuscaPalabra.java
@@ -0,0 +1,28 @@
+package Pablo;
+
+/**
+ * Clase que almacena una frase y permite comprobar
+ * si una palabra se encuentra dentro de ella.
+ */
+public class BuscaPalabra {
+    private String frase;
+
+    /**
+     * Constructor de la clase BuscaPalabra.
+     *
+     * @param frase Frase en la que se va a buscar la palabra.
+     */
+    public BuscaPalabra(String frase) {
+        this.frase = frase;
+    }
+
+    /**
+     * Busca una palabra dentro de la frase.
+     *
+     * @param palabra Palabra que se desea buscar.
+     * @return true si la palabra se encuentra en la frase, false en caso contrario.
+     */
+    public boolean buscarPalabra(String palabra) {
+        return frase.indexOf(palabra) != -1;
+    }
+}
---
+++ src/Pablo/BuscaVecesPalabra.java
@@ -0,0 +1,36 @@
+package Pablo;
+
+/**
+ * Clase que almacena una frase y permite contar
+ * cuántas veces aparece una palabra en ella.
+ */
+public class BuscaVecesPalabra {
+    private String frase;
+
+    /**
+     * Constructor de la clase BuscaVecesPalabra.
+     *
+     * @param frase Frase sobre la que se realizará la búsqueda.
+     */
+    public BuscaVecesPalabra(String frase) {
+        this.frase = frase;
+    }
+
+    /**
+     * Cuenta cuántas veces aparece una palabra dentro de la frase.
+     *
+     * @param palabra Palabra que se desea buscar.
+     * @return Número de veces que aparece la palabra; si no aparece, devuelve 0.
+     */
+    public int contarVecesPalabra(String palabra) {
+        int contador = 0;
+        int posicion = 0;
+
+        while (frase.indexOf(palabra, posicion) != -1) {
+            contador++;
+            posicion = frase.indexOf(palabra, posicion) + palabra.length();
+        }
+
+        return contador;
+    }
+}
